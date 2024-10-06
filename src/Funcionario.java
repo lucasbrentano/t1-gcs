9ca040ae095d220d9a7a6a7832ec2f0cd1bf13e9
Fitshow <lfelipefacosta@hotmail.com>
2024-10-06 00:20:01 -0300

    Funcionario implementado

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Funcionario extends Usuario{
 
+    public Departamento getDepartamento() {
+        return departamento;
+    }
 }
