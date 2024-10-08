0e426c50fbf03d1bab7cbf35c4cde379c323a49f
LuizK <kubaszewski001@hotmail.com>
2024-10-08 18:28:25 -0300

    Adicionando a verificação que o pedido só pode ser fechado caso esteja com o status aberto

--- src/Empresa.java
+++ src/Empresa.java
@@ -45,8 +45,6 @@ public class Empresa {
             // d.addFuncionario(f);
         }
     }
-
-
     public void executa() {
         this.initDepartamentosIniciais();
 
@@ -55,22 +53,24 @@ public class Empresa {
             scanner.nextLine();
 
             if (usuarioAtivo == null){
-                 // TODO trocaUsuario();
+                trocaUsuario();
             }
 
             if (usuarioAtivo instanceof Administrador) {
+                menuAdministrador();
                 switch (opcao) {
                     case 1:
-                        // TODO trocaUsuario();
+                        trocaUsuario();
                         break;
                     case 2:
                         // TODO registraPedido();
                         break;
                     case 3:
                         // TODO excluiPedido();
+                        excluiPedido();
                         break;
                     case 4:
-                        // TODO avaliaPedido();
+                        avaliaPedido();
                         break;
                     case 5:
                         // TODO listaPedidos();
@@ -88,15 +88,17 @@ public class Empresa {
                         break;
                 }
             } else {
+                menuFuncionario();
                 switch (opcao) {
                     case 1:
-                        // TODO trocaUsuario();
+                        trocaUsuario();
                         break;
                     case 2:
                         // TODO registraPedido();
                         break;
                     case 3:
                         // TODO excluiPedido();
+                        excluiPedido();
                         break;
                     default:
                         break;
@@ -105,6 +107,134 @@ public class Empresa {
         }
     }
 
+    public void menuAdministrador(){
+        System.out.println("╔══════════════════════════════════════════════════════╗");
+        System.out.println("║                     MENU PRINCIPAL                   ║");
+        System.out.println("╠══════════════════════════════════════════════════════╣");
+        System.out.println("║ 1 - Trocar Usuário                                   ║");
+        System.out.println("║ 2 - Registrar Pedido                                 ║");
+        System.out.println("║ 3 - Exclui Pedido                                    ║");
+        System.out.println("║ 4 - Avalia Pedido                                    ║");
+        System.out.println("║ 6 - Busca Pedidos Por Funcionario                    ║");
+        System.out.println("║ 7 - Busca Pedidos Por Descricao                      ║");
+        System.out.println("║ 8 - Mostra Estatisticas                              ║");
+        System.out.println("╚══════════════════════════════════════════════════════╝");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    public void menuFuncionario(){
+        System.out.println("╔══════════════════════════════════════════════════════╗");
+        System.out.println("║                     MENU PRINCIPAL                   ║");
+        System.out.println("╠══════════════════════════════════════════════════════╣");
+        System.out.println("║ 1 - Trocar Usuário                                   ║");
+        System.out.println("║ 2 - Registrar Pedido                                 ║");
+        System.out.println("║ 3 - Exclui Pedido                                    ║");
+        System.out.println("╚══════════════════════════════════════════════════════╝");
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private void trocaUsuario() { 
+        System.out.println("Digite o nome do usuario");
+        int codigoUsuario = scanner.nextInt();
+
+        for(Usuario u : usuarios){
+            if(u.getId() == codigoUsuario){
+                usuarioAtivo = u;
+                return;
+            }
+        }
+
+    }
+
+    private void excluiPedido() {
+        Pedido pedidoParaExcluir = null;
+        Departamento departamentoPedido = null;
+        for (Departamento d : departamentos) {
+            for (Pedido p : d.getPedidos()) {
+                if (p.getFuncionario().equals(usuarioAtivo) && p.getStatus().equals(Status.ABERTO)) ;
+                pedidoParaExcluir = p;
+                departamentoPedido = d;
+                break;
+            }
+
+        }
+
+        if (pedidoParaExcluir != null && departamentoPedido != null) {
+            departamentoPedido.getPedidos().remove(pedidoParaExcluir);
+            System.out.println("Pedido excluido com sucesso!");
+
+        } else {
+            System.out.println("Pedido não encontrado ou não pode ser excluído.");
+        }
+    }
+
+    private void avaliaPedido() {
+        System.out.println("Informe a ID do pedido: ");
+        int codigoPedido = scanner.nextInt();
+        scanner.nextLine();
+        for (Departamento d : departamentos) {
+            if (((Funcionario)usuarioAtivo).getDepartamento().equals(d)) {
+                for (Pedido p : d.getPedidos()) {
+                    if (p.getId() == codigoPedido && p.getStatus().equals(Status.ABERTO)) {
+                        System.out.println("Pedido " + p.getId() + " encontrado. \n" +
+                                            p.toString() +
+                                            "\nSelecione uma opção: ");
+                        System.out.println("1 - Aprovar pedido.");
+                        System.out.println("2 - Reprovar pedido.");
+                        System.out.println("3 - Sair");
+                        int opcao = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (opcao) {
+                            case 1:
+                                p.setStatus(Status.APROVADO);
+                                System.out.println("Pedido aprovado com sucesso!");
+                                break;
+                            case 2:
+                                p.setStatus(Status.REPROVADO);
+                                System.out.println("Pedido reprovado com sucesso!");
+                                break;
+                            default:
+                                break;
+                        }
+                    } else if (p.getId() == codigoPedido && !p.getStatus().equals(Status.ABERTO)) {
+                        System.out.println("Pedido " + p.getId() + " encerrado.");
+                    } else {
+                        System.out.println("Pedido não encontrado!");
+                    }
+                }
+            }
+        }
+    }
+
+    private void buscaPedidosPorFuncionario(){
+        System.out.println("Digite o codigo do usuario");
+        int codigoUsuario = scanner.nextInt();
+        scanner.nextLine();
+        Funcionario usuario = null;
+        for(Usuario u : usuarios){
+            if(u.getId() == codigoUsuario){
+                usuario = (Funcionario) u;
+                break;
+            }
+        }
+
+        if(usuario == null){
+            System.out.println("Usuario não encontrado");
+            return;
+        }
+
+        List<Pedido> pedidosDepartamento = usuario.getDepartamento().getPedidos();
+
+        List<Pedido> pedidoUsuario = ((Administrador) usuarioAtivo).buscarPedidosPorFuncionario(pedidosDepartamento,usuario);
+
+        for(Pedido p : pedidoUsuario){
+            System.out.println("╔══════════════════════════════════════════════════════╗");
+            System.out.println("║                      PEDIDO                          ║");
+            System.out.println("╚══════════════════════════════════════════════════════╝");
+
+            System.out.println(p);
+        }
+    }
 }
 
 //Fazer com que seja possível definir o usuário ativo.
